cc1be4a3a7ba3ecd2e97894547261ca4bc67ee35
Vic Segen <vic.segen@gmail.com>
2026-07-08 23:44:26 +0200

    test big file again

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -7,7 +7,7 @@ import { useVideoTexture } from '@react-three/drei'
 export default function Experience ()
 {
 
-    const videoTexture = useVideoTexture('/walktrim.mp4')
+    const videoTexture = useVideoTexture('/walk.mp4')
     console.log(videoTexture)
 
     return <>
