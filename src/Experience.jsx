b03ff7f642aaba034dade8d83e75335beeeea148
Vic Segen <vic.segen@gmail.com>
2026-07-23 13:08:27 +0200

    there is a sphere now + leva
    meshTransmissionMaterial

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,9 +1,10 @@
 import {useThree, extend, useFrame} from '@react-three/fiber'
 import {useRef} from 'react'
-import { useVideoTexture } from '@react-three/drei'
+import { useVideoTexture, Text, MeshTransmissionMaterial, Sphere } from '@react-three/drei'
 import fragmentShader from './shaders/blur/fragment.glsl'
 import vertexShader from './shaders/blur/vertex.glsl'
 import * as THREE from 'three'
+import { useControls } from 'leva'
 
 
 export default function Experience ()
@@ -22,11 +23,22 @@ export default function Experience ()
         }
     })
 
+    const materialProps = useControls({
+        thickness: { value: 0.2, min: 0, max: 3, step: 0.05},
+        roughness: { value: 0, min: 0, max: 1, step: 0.1},
+        transmission: { value: 1, min: 0, max: 1, step: 0.1},
+        ior: { value: 1.2, min: 0, max: 3, step: 0.1 },
+        chromaticAberration: { value: 0.02, min: 0, max: 1},
+        backside: { value: true}
+    })
+
     return <>
 
     <mesh ref={meshRef}>  
             <planeGeometry args={[4, 3]} />
             <meshBasicMaterial map={videoTexture} />
+            <Text> Bye Kiwi</Text>
+            <Sphere><MeshTransmissionMaterial {...materialProps}/></Sphere>
             <shaderMaterial
             uniforms={{ uTexture: { value: videoTexture }, uTintColor: { value: [0.2, 0.5, 0.5] }, uTime: { value: 0 }, uFrequency: { value: new THREE.Vector2(10, 5) }}}
             fragmentShader={fragmentShader}
