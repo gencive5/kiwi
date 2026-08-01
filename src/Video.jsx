57b4ad3aff92a29178b00d254f030211e010edf5
Vic Segen <vic.segen@gmail.com>
2026-08-01 15:51:02 +0200

    mute unmute button 🐒

--- src/Video.jsx
+++ src/Video.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from 'react'
 import './style.css'
 
 export default function Video() {
 
+    
+    const [muted, setMuted] = useState(true);
+    const handleToggleMute = () => setMuted(current => !current);
+
     return(
         <div>
-            <video loop autoPlay muted id="bg-video">
+            <video loop autoPlay muted={muted} playsInline id="bg-video">
                  <source src="/walk.mp4" type="video/mp4" />
             </video>
+
+            <button onClick={handleToggleMute} className="control">{muted ? "Unmute" : "Mute"}</button>
         </div>
     )
 }
+
+
+
+                
